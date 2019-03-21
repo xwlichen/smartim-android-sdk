97c71b724918db4e397f7395cf339e237ffc19b8
lichen <1960003945@qq.com>
2019-03-21 18:11:00 +0800

    初步提交

---
+++ app/src/main/java/com/smart/im/android/sdk/core/UDPSocketProvider.java
@@ -0,0 +1,10 @@
+package com.smart.im.android.sdk.core;
+
+/**
+ * @date : 2019/3/21 下午5:12
+ * @author: lichen
+ * @email : 1960003945@qq.com
+ * @description :
+ */
+public class UDPSocketProvider {
+}
